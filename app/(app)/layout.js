43df14d192eeaee098d8b069a0cfcac6660ae264
fendnugraha <fendnugraha92@gmail.com>
2026-08-17 12:10:24 +0700

    fix admin login

--- app/(app)/layout.js
+++ app/(app)/layout.js
@@ -8,36 +8,35 @@ import SessionVerifier from "./SessionVerifier";
 import AccessDeniedScreen from "./AccessDeniedScreen";
 
 const AppLayout = ({ children }) => {
-  useEffect(() => {
-    getUserGeoLocation(); // kirim sekali
+    useEffect(() => {
+        getUserGeoLocation(); // kirim sekali
 
-    const interval = setInterval(
-      () => {
-        getUserGeoLocation();
-      },
-      5 * 60 * 1000,
+        const interval = setInterval(
+            () => {
+                getUserGeoLocation();
+            },
+            5 * 60 * 1000,
+        );
+
+        return () => clearInterval(interval);
+    }, []);
+    // Hook akan otomatis mengurus redirect ke / atau ke /portal-checkin lewat useEffect di dalamnya
+    const { user, authLoading, logout } = useAuth({ middleware: "auth" });
+    if (authLoading) {
+        return <SessionVerifier />;
+    }
+
+    // 🔥 UPDATE: bypass menggunakan user.role langsung
+    const isAllowed = user && (user.has_checked_in || ["Administrator", "Super Admin"].includes(user.role));
+    if (!isAllowed) {
+        return <AccessDeniedScreen />;
+    }
+    return (
+        <div className="flex h-screen overflow-hidden">
+            <Navigation logout={logout} user={user} />
+            <div className="flex-1">{children}</div>
+        </div>
     );
-
-    return () => clearInterval(interval);
-  }, []);
-  // Hook akan otomatis mengurus redirect ke / atau ke /portal-checkin lewat useEffect di dalamnya
-  const { user, authLoading, logout } = useAuth({ middleware: "auth" });
-  if (authLoading) {
-    return <SessionVerifier />;
-  }
-
-  // 🔥 UPDATE: bypass menggunakan user.role langsung
-  const isAllowed =
-    user && (user.has_checked_in || user.role === "Super Admin");
-  if (!isAllowed) {
-    return <AccessDeniedScreen />;
-  }
-  return (
-    <div className="flex h-screen overflow-hidden">
-      <Navigation logout={logout} user={user} />
-      <div className="flex-1">{children}</div>
-    </div>
-  );
 };
 
 export default AppLayout;
